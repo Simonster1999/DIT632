3c95174065b404d54a5c659aa678ead7418d8d15
Mattias Ekdahl <matekd@student.chalmers.se>
2021-02-02 00:38:28 +0100

    Make 2_2 work

---
+++ Workpackage 2/Exerc_2_2/exerc_2_2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define MAX 10
+#define UPPER 99
+#define LOWER 1
+
+int sum(int* array, int size);
+
+void main() {
+
+    srand(time(0));
+
+    int array[MAX];
+
+    for(int i = 0; i < MAX; i++) {
+        array[i] = rand() % UPPER + LOWER;
+    }
+
+    printf("\nThe value of the label array (address) is: %p", &array);
+    printf("\nFirst integer in the array is (array[0]) : %d", array[0]);
+    printf("\nThe size of an integer (number of bytes) is : %d", sizeof(array[0]));
+    printf("\nThe size of the whole array is : %d\n", sum(array, MAX));
+
+    for(int i = 0; i < MAX; i++) {
+        int* num = &array[i];
+        printf("[%i] value: %d, double: %d\n", i, *num, *num * 2);
+        // printf("[%i] value: %d, double: %d\n", i, array[i], array[i] * 2);
+    }
+
+}
+
+int sum(int* array, int size) {
+    int sum = 0;
+    for(int i = 0; i < size; i++) {
+        sum += *array;
+        array++;
+    }
+    return sum;
+}
