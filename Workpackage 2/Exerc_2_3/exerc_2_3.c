afccbabc9546d71c6ba82801b6df7d410710762c
Emil Gustafsson <emgusta@student.chalmers.se>
2021-02-03 23:53:51 +0100

    Exercise 2_5 and comments on 2_3 and 2_4

--- Workpackage 2/Exerc_2_3/exerc_2_3.c
+++ Workpackage 2/Exerc_2_3/exerc_2_3.c
@@ -1,8 +1,23 @@
+// include section
 #include <stdio.h>
 #include <string.h>
 
+// function declaration
 int cmp();
 int nocmp();
+
+// Main program section
+
+/**
+ * This program receives strings and checks whether they are equal or not.
+ * 
+ * 
+ * Purpose: Demonstration of Exerc_2_3
+ * DIT632
+ * 
+ * Author: Emil Gustafsson, 2021
+ * 
+**/
 
 int main()
 {
@@ -10,33 +25,44 @@ int main()
 	nocmp();
 }
 
+// compare strings using strcmp
 int cmp() {
 	char a[] = "Hello";
 	char b[] = "Hiya";
+	// compare string a to b
 	int comparison = strcmp(a, b);
+	// print this if strcmp returns 0
 	if (comparison == 0) {
 		printf("String a and string b are equal.\n");
 	}
+	// print this if strcmp returns -1 or 1
 	else {
 		printf("String a and string b are not equal.\n");
 	}
 }
 
+// compare strings without using strcmp
 int nocmp() {
 	char a[] = "Hello";
 	char b[] = "Hello";
 	int isequal;
+	// loop through every letter of  the string
 	for (int i = 0; i < (sizeof(a)); i++) {
+		// compare every corresponding letter in the other string to current index and set isequal to 1 if they are the same
 		if (a[i] == b[i]) {
 			isequal = 1;
 		}
+		// if they aren't the same, set isequal to 0 and break out of the loop
 		else {
 			isequal = 0;
+			break;
 		}
 	}
+	// print this if isequal equals 1
 	if (isequal == 1) {
 		printf("String a and string b are equal.");
 	}
+	// print this if isequal equals any other value
 	else {
 		printf("String a and string b are not equal.");
 	}
--- Workpackage 2/Exerc_2_4/exerc_2_4.c
+++ Workpackage 2/Exerc_2_4/exerc_2_4.c
@@ -1,27 +1,49 @@
+// include section
 #include <stdio.h>
+
+// Main program section
+
+/**
+ * This program checks whether or not a string is a palindrome.
+ * 
+ * 
+ * Purpose: Demonstration of Exerc_2_4
+ * DIT632
+ * 
+ * Author: Emil Gustafsson, 2021
+ * 
+**/
 
 int main()
 {
-	char inputString[] = "";
+	char inputString[] = "";	// string to store user input in
 	int isPalindrome;
 
 	printf("Enter a string to find out whether or not it's a palindrome:\n");
+	// get user input
 	gets(inputString);
 	
+	// declare variables to store start and end of the user input string
 	int start = 0;
 	int end = strlen(inputString) - 1;
 
+	// compare start and end
 	for (int i = 0; i < (sizeof(inputString)); i++) {
+		// for every loop, increase start by 1 and decrease end by 1
 		if (inputString[start++] != inputString[end--]) {
+			// isPalindrome will be set to 0 every time the loop finds a set of non-matching characters
 			isPalindrome = 0;
 		}
 		else {
+			// if the compared characters do match, set isPalindrome to 1
 			isPalindrome = 1;
 		}
 	}
+	// print this if isPalindrome equals 0
 	if (isPalindrome == 0) {
 		printf("This string is not a palindrome.\n");
 	}
+	// print this is isPalindrome has any other value than 0
 	else {
 		printf("This string is a palindrome.\n");
 	}
