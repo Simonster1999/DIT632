a241639d103a2d3dae7493a03943db7445f6cb94
Mattias Ekdahl <matekd@student.chalmers.se>
2021-02-03 21:09:28 +0100

    Add comments

--- Workpackage 2/Exerc_2_1/exerc_2_1.c
+++ Workpackage 2/Exerc_2_1/exerc_2_1.c
@@ -1,35 +1,75 @@
+/*====================================
+File name : exerc_2_1.c
+Date : 2021 - 02 - 04
+Group nr 7
+Members that contribute to the solutions
+Mattias Ekdahl
+Member not present at demonstration time :
+---
+Demonstration code : []
+====================================== */
+
+// Include section
 #include <stdio.h>
 #include <string.h>
 
-#define MAX 20
+// Define section
+#define MAX 20      // defines the maximum number of chars to read
 
+// Function prototype declaration
 void copyString(char* target, char* origin);
+
+// Main program section
+
+/**
+ * This program showcases how to copy a string using two functions,
+ * also using the terminal and a seperate file as input.
+ * 
+ * Purpose: Demonstration of Exerc_2_1
+ * DIT632
+ * 
+ * Author: Mattias Ekdahl, 2021
+ * 
+**/
 
 void main() {
     
-    char str[MAX];
-    char strCopy[MAX];
+    // variable declarations
+    char str[MAX];      // Original string
+    char strCopy[MAX];  // Copy of original string
 
+    // program logic
+
+    // get the string, either from console or separete file
     printf("Type the string to copy: ");
     scanf("%20s", str);
     
+    // copy the string using function from string.h
     strcpy(strCopy, str);
     printf("\nstrcpy: %s\n", strCopy);
 
-    strCopy[0] = '\0';  // make the two strings unequal again, only the first index though
+    // make the two strings unequal again, to show that section function works
+    strCopy[0] = '\0';
 
+    // copy string using own function
     copyString(strCopy, str);
     printf("copyString: %s\n", strCopy);
 }
 
+// method that copies a string from one pointer to another
 void copyString(char* target, char* origin) {
 
+    // loops through entire string
     for(int i = 0; i < MAX; i++) {
 
+        // finds end of string
         if (*origin != '\0') {
 
+            // copies one char at a time
             *target = *origin;
-            origin++, target++; // index pointers
+
+            // change index of pointers by one
+            origin++, target++;
         } else {
             break;
         }
--- Workpackage 2/Exerc_2_2/exerc_2_2.c
+++ Workpackage 2/Exerc_2_2/exerc_2_2.c
@@ -1,29 +1,62 @@
+/*====================================
+File name : exerc_2_2.c
+Date : 2021 - 02 - 04
+Group nr 7
+Members that contribute to the solutions
+Mattias Ekdahl
+Member not present at demonstration time :
+---
+Demonstration code : []
+====================================== */
+
+// Include section
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 
-#define MAX 10
-#define UPPER 99
-#define LOWER 1
+// Define section
+#define MAX 10      // defines maximum numbers to generate
+#define UPPER 99    // defines the highest number that can be generated
+#define LOWER 1     // defines the lowest number that can be generated
 
+// Function prototype declaration
 int sum(int* array, int size);
+
+// Main program section
+
+/**
+ * This program uses a random number generator to fill an array and prints info about it
+ * and its content
+ * 
+ * Purpose: Demonstration of Exerc_2_2
+ * DIT632
+ * 
+ * Author: Mattias Ekdahl, 2021
+ * 
+**/
 
 void main() {
 
+    // variable declarations
+    int array[MAX];
+    
+    // program logic
+
+    // generating the first random number
     srand(time(0));
 
-    int array[MAX];
-
+    // generate a random number for each index using i and MAX
     for(int i = 0; i < MAX; i++) {
         array[i] = rand() % UPPER + LOWER;
     }
 
     printf("\nThe value of the label array (address) is: %p", &array);
     printf("\nFirst integer in the array is (array[0]) : %d", array[0]);
-    printf("\nThe size of an integer (number of bytes) is : %d", sizeof(array[0]));
-    printf("\nThe size of the whole array is : %d", sizeof(array));
-    printf("\nThe sum of the all values is : %d\n", sum(array, MAX));
+    printf("\nThe size of an integer (number of bytes) is : %d", sizeof(array[0])); // size of first element
+    printf("\nThe size of the whole array is : %d", sizeof(array));                 // size of array
+    printf("\nThe sum of the all values is : %d\n", sum(array, MAX));               // sum of array
 
+    // prints each element and its double value
     for(int i = 0; i < MAX; i++) {
         int* num = &array[i];
         printf("[%i] value: %d, double: %d\n", i, *num, *num * 2);
@@ -31,6 +64,7 @@ void main() {
 
 }
 
+// returns the sum of an array
 int sum(int* array, int size) {
     int sum = 0;
     for(int i = 0; i < size; i++) {
