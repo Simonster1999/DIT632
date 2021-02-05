7a547d43444c4c68f40ab4c93dcd12ad6a009585
Emil Gustafsson <emgusta@student.chalmers.se>
2021-02-05 14:13:44 +0100

    Update comments on 2_3

--- Workpackage 2/Exerc_2_3/exerc_2_3.c
+++ Workpackage 2/Exerc_2_3/exerc_2_3.c
@@ -16,8 +16,8 @@ Demonstration code : [FQ74-RF3M-MB8V-45W6]
 #include <string.h>
 
 // function declaration
-int cmp();
-int nocmp();
+void cmp(char *input, char *input2);
+void nocmp(char *input, char *input2);
 
 // Main program section
 
@@ -32,63 +32,71 @@ int nocmp();
  *
 **/
 
-int main(int argc, char* argv[])
+// main receives strings from command line and stores them in argv. argc keeps track of how many strings
+void main(int argc, char *argv[])
 {
-	// ./exerc_2_3.exe hello world		must be compiled before, .exe must exsist
-	// printf("%s", argv[1]);
-	// printf("%d", (int) argc);
-	if (argc == 3) {
+	// ./exerc_2_3.exe hello world		must be compiled before, .exe must exist
+	// checks argc passed into main, and if it's exactly 3, run the two functions (3, not 2 because file name counts as a passed string)
+	if (argc == 3)
+	{
+		// the second and third string is passed into the functions
 		cmp(argv[1], argv[2]);
 		nocmp(argv[1], argv[2]);
-	} else {
-		printf("Must write excatly two strings");
+		// program ends if argc is not exactly 3
 	}
-	
+	else
+	{
+		printf("Must write exactly two strings");
+	}
 }
 
 // compare strings using strcmp
-int cmp(char* input, char* input2) {
-	//char a[] = "Hello";
-	//char b[] = "Hiya";
-	// compare string a to b
+void cmp(char *input, char *input2)
+{
+	// variable containing the comparison between the strings
 	int comparison = strcmp(input, input2);
 	// print this if strcmp returns 0
-	if (comparison == 0) {
+	if (comparison == 0)
+	{
 		printf("These strings are equal.\n");
 	}
 	// print this if strcmp returns -1 or 1
-	else {
+	else
+	{
 		printf("These strings are not equal.\n");
 	}
 }
 
 // compare strings without using strcmp
-int nocmp(char* input, char* input2) {
-	//char a[] = "Hello";
-	//char b[] = "Hello";
+void nocmp(char *input, char *input2)
+{
 	int isequal = 1;
 	// loop through every letter of  the string
-	//for (int i = 0; i < (sizeof(input[0])); i++) {
-		// compare every corresponding letter in the other string to current index and set isequal to 1 if they are the same
-	if (sizeof(input) == sizeof(input2)) {
-		for (int i = 0; i < sizeof(input); i++) {
-			if (input[i] != input2[i]) {
+	// compare every corresponding letter in the other string to current index and set isequal to 1 if they are the same
+	if (sizeof(input) == sizeof(input2))
+	{
+		for (int i = 0; i < sizeof(input); i++)
+		{
+			if (input[i] != input2[i])
+			{
 				isequal = 0;
 			}
-			// if they aren't the same, set isequal to 0 and break out of the loop
+			// if they aren't the same, set isequal to 0
 		}
-	} else {
+	}
+	else
+	{
 		isequal = 0;
 	}
-	
+
 	// print this if isequal equals 1
-	if (isequal == 1) {
+	if (isequal == 1)
+	{
 		printf("These strings are equal.");
 	}
 	// print this if isequal equals any other value
-	else {
+	else
+	{
 		printf("These strings are not equal.");
 	}
-
-	
 }
