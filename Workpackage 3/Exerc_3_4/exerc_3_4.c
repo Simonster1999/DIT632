73e34a48043b1083b402beb888e22fa6cdd21a52
simengs <simengs@student.chalmers.se>
2021-02-12 19:07:55 +0100

    kommentarer

--- Workpackage 3/Exerc_3_4/exerc_3_4.c
+++ Workpackage 3/Exerc_3_4/exerc_3_4.c
@@ -1,3 +1,16 @@
+/*====================================
+File name : exerc_3_4.c
+Date : 2021 - 02 - 12
+Group nr 7
+Members that contribute to the solutions
+Mattias Ekdahl
+Emil Gustafsson
+Simon Engström
+Member not present at demonstration time :
+---
+Demonstration code : [QEGF-Z6Z6-5V4G-WULS]
+====================================== */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -16,18 +29,37 @@ void write_new_file(PERSON *inrecord); //Creats a file and write a first record
 void printfile(void);                  // print out all persons in the file
 void search_by_firstname(char *name);  // print out person if in list
 void append_file(PERSON *inrecord);    // appends a new person to the file
-void search_by_lastname(char *name);
+void search_by_lastname(char *name);   //Searchs by last name and prints out people with that lastname
+
+//global variables
+FILE *file;
+char *fileName = "Person_register.bin"; //declaring the the file name and type to use when reading and writing to file
+
+//main program section
+
+/**
+ * This program lets you write person data to a binary file, 
+ * and also let search and print the stored data in the file
+ * to the console
+ *
+ *
+ * Purpose: Demonstration of Exerc_3_4
+ * DIT632
+ *
+ * Author: Simon Engström, 2021
+ *
+**/
 
 int main(void)
 {
-  int ans;
-  char name[20];
-  PERSON input;
+  PERSON person;
   PERSON ppost;
-  strcpy(ppost.firstname, "Dummy");
-  strcpy(ppost.famnamne, "LastName");
+  strcpy(ppost.firstname, "Dummy");    //creating a dummy record containing hardcoded info that is
+  strcpy(ppost.famnamne, "LastName");  //inserted into the file upon creation
   strcpy(ppost.pers_number, "123456");
   int userInput;
+  int ans;
+  char name[20];
 
   printf("\n***Person Register***\n1. Write new File \n2. Register person \n3. Search Person \n4. List all registered persons\n5. Exit\n");
 
@@ -41,16 +73,16 @@ int main(void)
     break;
   case 2:
     printf("Enter first name: \n");
-    scanf("%20s", input.firstname);
+    scanf("%20s", person.firstname);
     printf("Enter last name: \n");
-    scanf("%20s", input.famnamne);
+    scanf("%20s", person.famnamne);
     printf("Enter SSN: \n");
-    scanf("%13s", input.pers_number);
-    append_file(&input);
+    scanf("%13s", person.pers_number);
+    append_file(&person);    //creates a new record with the data the user entered in the last index in the file
     main();
     break;
   case 3:
-    printf("Search by first name(1), or Search by last name(2)?");
+    printf("Search by first name(1), or Search by last name(2)?\n");
     scanf("%d", &ans);
     if (ans == 1)
     {
@@ -83,65 +115,65 @@ int main(void)
   }
 }
 
+//method that recieves a hardcoded dummy record, then either creates a binary file in the same location as the .exe, or overwrites a currently
+//existing one and writes the dummy record to the file.
+
 void write_new_file(PERSON *inrecord)
 {
-  FILE *file;
-  char *fileName = "C:\\Users\\Admin\\Desktop\\Ny mapp\\Person_register.txt";
 
-  file = fopen(fileName, "wb");
+  file = fopen(fileName, "wb");    //here we specify that we want to open the file in write mode(w), as well an binary(b)
 
-  if (file != NULL)
+  if (file != NULL)                //to make sure that the file has been succsesfully open, else we print "error"
   {
     fwrite(inrecord, sizeof(struct PERSON), 1, file);
   }
   else
   {
-    printf("ERROR");
+    printf("ERROR could not open");
   }
-  fclose(file);
+  fclose(file);                   //we close the file once we are done
 }
 
+//method that creates a temporary file and writes a record to it, then reads from that file and appends it to the last line of our
+//main file. and lastly deletes the newly created temporary file.
 void append_file(PERSON *inrecord)
 {
-  FILE *file1;
-  char *fileName1 = "C:\\Users\\Admin\\Desktop\\Ny mapp\\Person_register.txt";
   FILE *file2;
-  char *fileName2 = "C:\\Users\\Admin\\Desktop\\Ny mapp\\Person_register2.txt";
+  char *fileName2 = "Person_register2.bin";    //declaring the name and type of the temporary file
   PERSON temp;
 
-  file1 = fopen(fileName1, "ab");
-  file2 = fopen(fileName2, "wb");
+  file = fopen(fileName, "ab");                //here we open our main file in appedning mode
+  file2 = fopen(fileName2, "wb");              //and our temporary file in write mode
 
-  if (file1 != NULL && file2 != NULL)
+  if (file != NULL && file2 != NULL)           //check to see that both files open successfully
   {
-    fwrite(inrecord, sizeof(struct PERSON), 1, file2);
-    fclose(file2);
-    file2 = fopen(fileName2, "rb");
-    fread(&temp, sizeof(PERSON), 1, file2);
-    fwrite(&temp, sizeof(PERSON), 1, file1);
-  }
+    fwrite(inrecord, sizeof(struct PERSON), 1, file2);     //here we write the record to our temporary file
+    fclose(file2);                                         //we then close it
+    file2 = fopen(fileName2, "rb");                        //and open it again but in read mode
+    fread(&temp, sizeof(PERSON), 1, file2);                //we then read the temporary file, and get the data to our temp variable
+    fwrite(&temp, sizeof(PERSON), 1, file);                //we then write, or append the data from our temp variable to the last line
+  }                                                        //in our main file
   else
   {
     printf("ERROR");
   }
-  fclose(file1);
+  fclose(file);                                            //we then close both files
   fclose(file2);
-  remove("C:\\Users\\Admin\\Desktop\\Ny mapp\\Person_register2.txt");
+  remove("C:\\Users\\Admin\\Desktop\\Ny mapp\\Person_register2.txt");     //and lastly remove the temporary file
 }
 
+//method that print all of the records in our file into the console
 void printfile()
 {
-  FILE *file;
-  char *fileName = "C:\\Users\\Admin\\Desktop\\Ny mapp\\Person_register.txt";
   PERSON temp;
 
-  file = fopen(fileName, "rb");
+  file = fopen(fileName, "rb");     //here we open the file in read mode
 
   if (file != NULL)
   {
-    while (fread(&temp, sizeof(PERSON), 1, file) != 0)
+    while (fread(&temp, sizeof(PERSON), 1, file) != 0)      //we read from the file, and as long as we find a record we print it to the console
     {
-      printf("%20s %20s %20s \n", temp.firstname, temp.famnamne, temp.pers_number);
+      printf("%20s %20s %20s \n", temp.firstname, temp.famnamne, temp.pers_number);     
     }
   }
   else
@@ -151,20 +183,20 @@ void printfile()
   fclose(file);
 }
 
+//method that gets user input and checks if there is a record in the file that has the same firstname as the user input
+//if we find one or more matches we print it to the console
 void search_by_firstname(char *name)
 {
   PERSON temp;
-  FILE *file;
-  char *fileName = "C:\\Users\\Admin\\Desktop\\Ny mapp\\Person_register.txt";
 
   file = fopen(fileName, "rb");
 
   if (file != NULL)
   {
-    while (fread(&temp, sizeof(PERSON), 1, file) != 0)
+    while (fread(&temp, sizeof(PERSON), 1, file) != 0)     //reads the file as long as we find a record
     {
-      if (strcmp(temp.firstname, name) == 0)
-      {
+      if (strcmp(temp.firstname, name) == 0)               //if we find a record with a firstname that matches the user input we print the record
+      {                                                    //to the console
         printf("%20s %20s %20s \n", temp.firstname, temp.famnamne, temp.pers_number);
       }
     }
@@ -176,20 +208,20 @@ void search_by_firstname(char *name)
   fclose(file);
 }
 
+//method that gets user input and checks if there is a record in the file that has the same firstname as the user input
+//if we find one or more matches we print it to the console
 void search_by_lastname(char *name)
 {
   PERSON temp;
-  FILE *file;
-  char *fileName = "C:\\Users\\Admin\\Desktop\\Ny mapp\\Person_register.txt";
 
   file = fopen(fileName, "rb");
 
   if (file != NULL)
   {
-    while (fread(&temp, sizeof(PERSON), 1, file) != 0)
+    while (fread(&temp, sizeof(PERSON), 1, file) != 0)           //reads the file as long as we find a record
     {
-      if (strcmp(temp.famnamne, name) == 0)
-      {
+      if (strcmp(temp.famnamne, name) == 0)                      //if we find a record with a famname that matches the user input we print the record
+      {                                                          //to the console
         printf("%20s %20s %20s \n", temp.firstname, temp.famnamne, temp.pers_number);
       }
     }
