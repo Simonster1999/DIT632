19863e84e23fbaccbf92720c8fe17c1798f52cb6
Mattias Ekdahl <matekd@student.chalmers.se>
2021-02-19 14:04:28 +0100

    Add comments to 4_1

--- Workpackage 4/Exerc_4_1/code.c
+++ Workpackage 4/Exerc_4_1/code.c
@@ -1,22 +1,53 @@
+/*====================================
+File name : code.c
+Date : 2021 - 02 - 19
+Group nr 7
+Members that contribute to the solutions
+Mattias Ekdahl
+Emil Gustafsson
+Simon Engström
+Member not present at demonstration time :
+---
+Demonstration code : [VGP9-HVEG-QSCW-K7ZD]
+====================================== */
+
+// Include section
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// Main program section
+
+/**
+ * This program reads five arguments for five different values, the values are checked to be within
+ * the allowed ranges and are then packed into a byte and printed out as a hexadecimal number.
+ * 
+ * Purpose: Demonstration of Exerc_4_1
+ * DIT632
+ * 
+ * Author: Mattias Ekdahl, 2021
+ * 
+**/
+
 void main(int argc, char *argv[]) {
-        
-    // ./code 1 1 1 1 1
+
+    // variable declarations
+    unsigned char byte;     // stores the combined values
+    int sum[5];             // stores each individual value
+
+    // program logic
+
     // check for exactly 5 arguments
     if (argc == 6) {
         
-        unsigned char byte;
-        int sum[5];
-
+        // casts each string argument to an integer and turns '0-9' to 0-9 by subtracting 48
         for (int i = 0; i < 5; i++) {
 
             sum[i] = (int) *argv[i + 1] - 48;
         }
 
-        // within range
+        // check that the numbers provided do not fit within the ranges specified
+        // also exits if non numbers were provided, 'abc' for example
         if ((sum[0] != 0 && sum[0] != 1) || 
             (sum[1] >  4) || 
             (sum[2] >  2) || 
@@ -26,8 +57,8 @@ void main(int argc, char *argv[]) {
             printf("Invalid number provided, number out of allowed ranges\n");
             exit(1);
         }
-
-        // [128] [64 32 16] [8 4] [2] [1], bits grouped together
+        // all five numbers should be stored in one byte and has their own bits,
+        // the values must therefore be multplied to be moved to seperate bits.
         // when summing up the different values their position in the byte has to be taken into account
         // turning value to correct binary number then convert to decimal
         byte = (sum[0] * 128 
