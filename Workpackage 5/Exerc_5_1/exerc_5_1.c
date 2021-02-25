1c93d7d55270c2ff8acd0040fe2003ef48ade311
Mattias Ekdahl <matekd@student.chalmers.se>
2021-02-25 11:26:36 +0100

    Add comments to 5.1

--- Workpackage 5/Exerc_5_1/exerc_5_1.c
+++ Workpackage 5/Exerc_5_1/exerc_5_1.c
@@ -1,3 +1,16 @@
+/*====================================
+File name : exerc_5_1.c
+Date : 2021 - 02 - 25
+Group nr 7
+Members that contribute to the solutions
+Mattias Ekdahl
+Emil Gustafsson
+Simon Engström
+Member not present at demonstration time :
+---
+Demonstration code : [XNNW-YDBD-576B-5366]
+====================================== */
+
 /* --- Macros predefined for the compiler
 DDRB Data direction register B
 PORTB Outport B
@@ -7,6 +20,7 @@ PORTD Outport D
 PIND Inport D
 */
 
+// constants section
 const int ROW1 = 11;
 const int ROW2 = 10;
 const int ROW3 = 9;
@@ -16,35 +30,56 @@ const int COL2 = 6;
 const int COL3 = 5;
 const int COL4 = 4;
 
-char keynumber = 'X';
-bool keyhit = false;
+// variable declarations
+char keynumber = 'X';   // most resent key
+bool keyhit = false;    // has a key been pressed or not
+
+// main program section
+
+/**
+ * This program enables the use of a keypad using arduino usin interupts instead of polling.
+ *
+ *
+ * Purpose: Demonstration of Exerc_5_1
+ * DIT632
+ *
+ * Author: Emil Gustafsson, Mattias Ekdahl, Simon Engström, 2021
+ *
+**/
 
 void setup()
 {
+    // configuring the in and out ports
 	DDRD = 0x00;
     DDRB= 0xFF;
 
     Serial.begin(9600);
 
+    // creates a interupt which ckecks whether digital pin 2 is FALLING, aka going from 1 to 0
   	attachInterrupt(digitalPinToInterrupt(2), keyboardirq, FALLING);
 }
 
 void loop()
 {
+    // checks if key has been hit without polling
     if (keyhit) {
       
         Serial.println(keynumber);
+
+        // sets keyhit to false, allows for another key to be detected and read by the interupt
       	keyhit = false;
     }
 }
 
 char keyPress()
 {
+    // sets all rows to HIGH so each row can individually be set to LOW
   	digitalWrite(ROW1, HIGH);
   	digitalWrite(ROW2, HIGH);
   	digitalWrite(ROW3, HIGH);
   	digitalWrite(ROW4, HIGH);
 
+    // we write to the input rows, and read from the output columns
   	digitalWrite(ROW1, LOW);
     int r1c1 = digitalRead(COL1);
     int r1c2 = digitalRead(COL2);
@@ -73,12 +108,14 @@ char keyPress()
     int r4c4 = digitalRead(COL4);
   	digitalWrite(ROW4, HIGH);
   	
+    // sets all rows to LOW so the interupt can detect new key presses
   	digitalWrite(ROW1, LOW);
   	digitalWrite(ROW2, LOW);
   	digitalWrite(ROW3, LOW);
   	digitalWrite(ROW4, LOW);
   
-  
+    // for each combination of rows and columns we check if a connection has been made, 
+    // and return the corrisponding number/letter
     if (r1c1 == 0)
     {
         return '1';
@@ -149,11 +186,17 @@ char keyPress()
     }
 }
 
+// interupt method, triggers whenever a key is pressed
 void keyboardirq() {
   
+  // ckecks if a key has already been pressed, incase multiple interupts are triggered by switching
+  // rows to HIGH and LOW again
   if(!keyhit){
-    
+
+    // reads which key was pressed, assigns it to keynumber
     keynumber = keyPress();
+
+    // assigns keyhit to true indicating that a key was pressed
   	keyhit = true;
   }
 }
