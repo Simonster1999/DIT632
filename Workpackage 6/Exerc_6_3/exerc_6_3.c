c0ff885e1e99dfd51dec5116b45f4300559ee5ed
Mattias Ekdahl <matekd@student.chalmers.se>
2021-03-05 16:29:29 +0100

    Complete 6.3, add comments to 6.5, add comments to 6.5

--- Workpackage 6/Exerc_6_3/exerc_6_3.c
+++ Workpackage 6/Exerc_6_3/exerc_6_3.c
@@ -1,42 +1,78 @@
+/*====================================
+File name : exerc_6_3.c
+Date : 2021 - 03 - 05
+Group nr 7
+Members that contribute to the solutions
+Mattias Ekdahl
+Emil Gustafsson
+Simon Engström
+Member not present at demonstration time :
+---
+Demonstration code : [PZU6-9MDJ-TCWT-F4XY]
+====================================== */
+
+// Include section
+#include <stdio.h>
+
+// Define section
 #define ML13_Status 0x0B10
 #define ML13_Control 0x0B00
-
 // Status
-#define CLOSING     0b110000000
-#define OPENING     0b101000000
-#define S2          0b100100000
-#define S1          0b100010000
-#define SENSOR      0b100001000
-
+#define CLOSING 0b10000000
+#define OPENING 0b01000000
+#define S2      0b00100000
+#define S1      0b00010000
+#define SENSOR  0b00001000
 // Control
-#define CLOSE       0b000000010
-#define OPEN        0b000000001
+#define CLOSE   0b00000010
+#define OPEN    0b00000001
+
+// Main program section
+
+/**
+ * This program is a draft of a program controlling an automatic door which will open when it detects
+ * a person and will remain open for a while afterwards before closing again.
+ * 
+ * Purpose: Demonstration of Exerc_6_3
+ * DIT632
+ * 
+ * Author: Mattias Ekdahl, Emil Gustafsson, Simon Engström, 2021
+ * 
+**/
 
 void setup() {
 
-    CLOSE = 1;
-    attachInterrupt(digitalPinToInterrupt(SENSOR), sensorirq, HIGH);
+    // initially closes doors. changes bit 1 to 1 in control registry
+    ML13_Control = ML13_Control | CLOSE;
+
+    // attaches an interupt triggered by the sensor detecting a person turning bit 3 to 1, HIGH
+    attachInterrupt(digitalPinToInterrupt(ML13_Status & SENSOR), sensorirq, HIGH);
 }
 
 void main() {
     
+    // runs indefinitely
     while (1) {
         
     }
 } 
 
+// interupt method. if a person is detected by the sensor this method is continuously called
 void sensorirq() {
 
-    // if not open
-    if(S1 != 1) {
-        OPEN = 1;
+    // reads bit 4 of status registry. if bit is not 1, i.e. door is not open, enter if statement
+    if(ML13_Status & S1 != S1) {
+        
+        // turns bit 0 in control registry to 1
+        ML13_Control = ML13_Control | OPEN;
 
-        // while oopening
-        while(OPENING == 1) {}
+        // loop while bit 6 indicates that door is still opening by being 1, turns to 0 when fully open
+        while(ML13_Status & OPENING == OPENING) {}
     }
     
-    // delay before closing
+    // delay to keep the door open a while before closing
     for(int i = 0; i < 100; i++) {}
 
-    CLOSE = 1;
+    // tells door to begin closing by turning bit 1 to 1
+    ML13_Control = ML13_Control | CLOSE;
 }
